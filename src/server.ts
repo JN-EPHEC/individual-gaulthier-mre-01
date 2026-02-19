249b8d4b3f1068989a0c2ffaedd22908a1050bae
gaulthier-mre-01 <g.meurie@students.ephec.be>
2026-02-19 14:47:57 +0100

    Erreur Cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import userRoutes from "./routes/userRoutes.js";
 import { requestLogger } from "./middlewares/logger.js";
@@ -11,6 +12,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(requestLogger);
 app.use(express.static("public"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
